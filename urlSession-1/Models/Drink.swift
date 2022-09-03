068a3ff824b020d1ab1d609180c425e04efd4806
meetIgor <iigoor.s@gmail.com>
2022-09-03 13:36:41 +0300

    url sessions: fetch image, fetch data form json

---
+++ urlSession-1/Models/Drink.swift
@@ -0,0 +1,8 @@
+//
+//  Drink.swift
+//  urlSession-1
+//
+//  Created by igor s on 02.09.2022.
+//
+
+import Foundation
---
+++ urlSession-1/ViewControllers/ActionCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ActionCollectionViewCell.swift
+//  urlSession-1
+//
+//  Created by igor s on 02.09.2022.
+//
+
+import UIKit
+
+class ActionCollectionViewCell: UICollectionViewCell {
+    
+}
